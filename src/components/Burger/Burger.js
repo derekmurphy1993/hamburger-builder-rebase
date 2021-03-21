e228508be2b7940f3bfb4826eaf3f40c8461dfa7
Derek Murphy <derekmurphy1993@gmail.com>
2021-03-20 21:50:29 -0400

    132 output multiple build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,31 @@
 import React from 'react'
+
 import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    // gives an array of object keys, .map continues off this
+    .map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />
+    })
+    // constructs array with all (...) using JS Array() method
+    // making an array of ingredients
+  })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+    // reduce array into one array
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p> Please start building a burger! </p>
+    }
+
   return (
     <div className={classes.Burger}>
     <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
+    {transformedIngredients}
     <BurgerIngredient type="bread-bottom" />
-
     </div>
   )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,25 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {...}
+  // } using more *modern* method of state =
+
+  state = {
+    ingredients: {
+      salad: 2,
+      bacon: 1,
+      cheese: 2,
+      meat: 1
+    }
+  }
+
   render () {
     return (
       <Aux>
-      <Burger />
+      <Burger ingredients={this.state.ingredients} />
       <div> Build Controls </div>
       </Aux>
     )
